f0c341acda073ebc12354041835956efda81ed4d
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 04:39:51 +0800

    Repurposed to handle input from gl window

--- engine/events.py
+++ engine/events.py
@@ -3,34 +3,18 @@ class Input:
         self._keys_down = set()
         self._keys_pressed = set()
         self._keys_released = set()
- 
-    def key_down(self, key):
-        if key not in self.keys_down:
-            self._keys_down.add(key)
-            self._keys_pressed.add(key)
-            
-    def key_up(self, key):
-        if key in self.keys_down:
-            self._keys_down.remove(key)
-            self._keys_released.add(key)
+        self._prev_keys = set()
 
-    def is_down(self, key):
+    def update(self, current_keys):
+        self._keys_pressed = current_keys - self._keys_down
+        self._keys_released = self._keys_down - current_keys
+        self._keys_down = current_keys
+
+    def key_down(self, key):
         return key in self._keys_down
-    
-    @property
-    def keys_pressed(self):
-        return self._keys_pressed
-    
-    @property
-    def keys_down(self):
-        return self._keys_down
 
     def was_pressed(self, key):
         return key in self._keys_pressed
 
     def was_released(self, key):
         return key in self._keys_released
-
-    def reset(self):
-        self._keys_pressed.clear()
-        self._keys_released.clear()
