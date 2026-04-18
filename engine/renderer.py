61c16cbfc2f0bbff2c9576a45c0a09752410b241
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 04:40:37 +0800

    Added scene is null check

--- engine/renderer.py
+++ engine/renderer.py
@@ -2,12 +2,12 @@ class Renderer:
     def __init__(self, ctx):
         self.ctx = ctx
 
-    def render(self, scene):
+    def render(self, scene=None):
         self.ctx.clear(0.1, 0.1, 0.1)
         
         if scene is None:
             return
-        
+                
         for obj in scene.objects:
             obj.render()
         
