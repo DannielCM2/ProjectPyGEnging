ec388bd28d67f3639e236035146be8e64d4c17de
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 01:52:43 +0800

    Removed scene loading from entry point

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from engine.core import Game
-from scenes.level1 import Level1
-from scenes.level2 import Level2
+from engine.game import Game
 
 game = Game()
-
-game.scene_manager.load_scene(Level1)
-
 game.run()
