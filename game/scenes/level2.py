d40e5e45f5c8e11ee23966a697e4b22f7fe287ae
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 04:42:31 +0800

    test level

--- game/scenes/level2.py
+++ game/scenes/level2.py
@@ -1,3 +1,16 @@
-class Level2:
-    def __init__(self):
-        pass
+from game.game_objects.cube_3d import Cube3D
+from engine.scene import Scene
+import glfw
+
+class Level2(Scene):
+    def __init__(self, engine):
+        super().__init__(engine=engine)
+        self.on_load()
+
+    def on_load(self):
+        self.objects.append(Cube3D(self.engine.window.ctx))
+
+    def update(self, dt):
+        if self.input.was_pressed(glfw.KEY_ESCAPE):
+            print("Unloading Level 2")
+            self.scene_manager.unload_scene()
