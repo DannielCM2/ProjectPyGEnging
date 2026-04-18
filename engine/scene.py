aaa15dbb171bb7a2fb42caf6b07333c12314a26b
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 04:41:50 +0800

    Update Super class scene to include boilerplates

--- engine/scene.py
+++ engine/scene.py
@@ -2,7 +2,17 @@ class Scene:
     def __init__(self, engine):
         self.objects = []
         self.engine = engine
-        self.scene_objects()
+        self.input = engine.input
+        self.scene_manager = engine.scene_manager
 
-    def scene_objects(self):
+    def on_load(self):
+        pass
+    
+    def update(self, dt):
+        pass
+
+    def on_enter(self):
+        pass
+
+    def on_exit(self):
         pass
