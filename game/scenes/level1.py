923681bda92d304d40310dd6a06eb1ac0e4ad413
DannielCM <danniel.mojica@citycollegeoftagaytay.edu.ph>
2026-04-19 04:42:26 +0800

    Test level

--- game/scenes/level1.py
+++ game/scenes/level1.py
@@ -1,9 +1,17 @@
 from game.game_objects.cube_3d import Cube3D
 from engine.scene import Scene
+import glfw
 
 class Level1(Scene):
     def __init__(self, engine):
         super().__init__(engine=engine)
+        self.on_load()
 
-    def scene_objects(self):
+    def on_load(self):
         self.objects.append(Cube3D(self.engine.window.ctx))
+        print(self.objects)
+
+    def update(self, dt):
+        if self.input.was_pressed(glfw.KEY_ESCAPE):
+            print("Loading level2")
+            self.scene_manager.load_scene('level2')
